ae2eb1492300f989d592711c0c9d5450e4220147
ashishcode-stack <ashishkumarmaurya794@gmail.com>
2025-08-08 20:01:47 +0530

    function are the defined

--- random.js
+++ random.js
@@ -5,3 +5,38 @@ const guesslot=document.querySelector('.perviousgues')
 const result=document.querySelector('.rem')
 const lower_high=document.querySelector('.lower-hi')
 const start_over=document.querySelector('.holding-perviousguess')
+
+const p=document.createElement('progress')
+let prevguess=[]
+let numguess=1
+
+let playgame =true;
+if(playgame){
+    button.addEventListener('click',function(e){
+        e.preventDefault()
+      const guess=  parseInt(userinput.value)
+      validateGuess(guess)
+    })
+}
+
+function validateGuess(guess){
+    //  valdition of game
+     
+
+}
+function checkguess(guess){
+    //checking the guess
+}
+function dispalyguess(guess){
+
+}
+
+function displayMessGage(messgae){
+
+}
+function newgame(){
+
+}
+function endgame(){
+
+}
